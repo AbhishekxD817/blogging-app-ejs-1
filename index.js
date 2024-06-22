5ad805cfbcc29649a7a48953196ea1590cbc6695
Abhishek <ab2004ak@gmail.com>
2024-06-22 14:39:51 +0530

    Created fully functional Routes to get all posts, and view single post, post data is hardcoded no database is used

--- index.js
+++ index.js
@@ -27,10 +27,69 @@ app.listen(PORT,()=>{
 })
 
 
+const posts_data = [
+  {
+    "post_id": "post1",
+    "username": "john_doe",
+    "followers": 1532,
+    "following": 864,
+    "content": "I love exploring new places!",
+    "likes": 42,
+    "tags": ["travel", "adventure"]
+  },
+  {
+    "post_id": "post2",
+    "username": "sara_smith",
+    "followers": 2897,
+    "following": 420,
+    "content": "Enjoying the sunny weather today ☀️",
+    "likes": 98,
+    "tags": ["nature", "outdoors"]
+  },
+  {
+    "post_id": "post3",
+    "username": "alex_jones",
+    "followers": 431,
+    "following": 312,
+    "content": "Just finished a great book! Highly recommend it.",
+    "likes": 57,
+    "tags": ["books", "reading"]
+  },
+  {
+    "post_id": "post4",
+    "username": "emily_wilson",
+    "followers": 7214,
+    "following": 205,
+    "content": "Cooking up some delicious vegan recipes 🌱",
+    "likes": 321,
+    "tags": ["cooking", "vegan"]
+  },
+  {
+    "post_id": "post5",
+    "username": "mark_thompson",
+    "followers": 185,
+    "following": 53,
+    "content": "Excited for the upcoming basketball season!",
+    "likes": 15,
+    "tags": ["sports", "basketball"]
+  }
+]
 
+
+// setup routes
 
 app.get("/",(req,res)=>{
-    res.render("Home")
+    res.render("Home");
+})
+
+app.get("/posts",(req,res)=>{
+  res.render("Posts",{posts:posts_data});
+})
+
+app.get("/posts/:id",async(req,res)=>{
+  let {id} = req.params;
+  let post = await posts_data.find((post)=> post.post_id === id)
+  res.render("Post",{post})
 })
 
 
